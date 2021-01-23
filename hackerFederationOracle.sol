5422682dc4b329067e9f60452d0280ebece6b3b3
chunqizhi <1558763837@qq.com>
2021-01-23 11:01:57 +0800

    Change ExampleOracleSimple to HackerFederationOracle

--- hackerFederationOracle.sol
+++ hackerFederationOracle.sol
@@ -33,7 +33,7 @@ contract HackerFederationOracle {
         uint112 reserve0;
         uint112 reserve1;
         (reserve0, reserve1, blockTimestampLast) = _pair.getReserves();
-        require(reserve0 != 0 && reserve1 != 0, 'ExampleOracleSimple: NO_RESERVES'); // ensure that there's liquidity in the pair
+        require(reserve0 != 0 && reserve1 != 0, 'HackerFederationOracle: NO_RESERVES'); // ensure that there's liquidity in the pair
     }
 
     function update() external {
@@ -42,7 +42,7 @@ contract HackerFederationOracle {
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
 
         // ensure that at least one full period has passed since the last update
-        require(timeElapsed >= PERIOD, 'ExampleOracleSimple: PERIOD_NOT_ELAPSED');
+        require(timeElapsed >= PERIOD, 'HackerFederationOracle: PERIOD_NOT_ELAPSED');
 
         // overflow is desired, casting never truncates
         // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
@@ -59,7 +59,7 @@ contract HackerFederationOracle {
         if (token == token0) {
             amountOut = price0Average.mul(amountIn).decode144();
         } else {
-            require(token == token1, 'ExampleOracleSimple: INVALID_TOKEN');
+            require(token == token1, 'HackerFederationOracle: INVALID_TOKEN');
             amountOut = price1Average.mul(amountIn).decode144();
         }
     }
